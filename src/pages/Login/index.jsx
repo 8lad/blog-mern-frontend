99f575e1801f6a17f0eafe61c860cb276cd3a45d
Vladyslav Troian <vladyslav.troian@cyper.ca>
2023-01-19 09:32:42 +0200

    feature: added functional for autorization

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,67 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAuth, selectIsAuth } from '../../redux/slices/auth';
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import { useForm } from 'react-hook-form';
+import { Navigate } from 'react-router-dom';
 
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid }
+  } = useForm({
+    defaultValues: {
+      email: '',
+      passwordHash: ''
+    },
+  });
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  }
+
+  if (isAuth) {
+    return <Navigate to='/' />
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          {...register('email', { required: 'Please, type email' })}
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register('passwordHash', { required: 'Please, type password' })}
+          fullWidth
+        />
+        <Button size="large" type="submit" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
