2c03733b983e3deb498e0f1dbf1a5c512ecae41a
Vladyslav Troian <vladyslav.troian@cyper.ca>
2023-01-27 16:29:47 +0200

    fix: returned custom tabs component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,43 +12,41 @@ import { fetchPosts, fetchTags } from "../redux/slices/posts";
 export const Home = () => {
 	const dispatch = useDispatch();
 	const { posts, tags } = useSelector((state) => state.posts);
-	const { sorting } = posts;
 	const userData = useSelector((state) => state.auth.data);
 	const isPostsLoading = posts.status === "loading";
 	const isTagsLoading = tags.status === "loading";
 
 	useEffect(() => {
-		dispatch(fetchPosts(sorting));
+		dispatch(fetchPosts());
 		dispatch(fetchTags());
-	}, [dispatch, sorting]);
+	}, [dispatch]);
 
 	return (
 		<>
 			<CustomTabs />
 			<Grid container spacing={4}>
 				<Grid xs={8} item>
-					{(isPostsLoading ? [...Array(5)] : posts.items).map(
-						(obj, index) =>
-							isPostsLoading ? (
-								<Post key={index} isLoading />
-							) : (
-								<Post
-									key={obj._id}
-									id={obj._id}
-									title={obj.title}
-									imageUrl={
-										obj.imageUrl
-											? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
-											: NoImage
-									}
-									user={obj.user}
-									createdAt={obj.createdAt}
-									viewsCount={obj.viewsCount}
-									commentsCount={3}
-									tags={obj.tags}
-									isEditable={userData?._id === obj.user._id}
-								/>
-							)
+					{(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+						isPostsLoading ? (
+							<Post key={index} isLoading />
+						) : (
+							<Post
+								key={obj._id}
+								id={obj._id}
+								title={obj.title}
+								imageUrl={
+									obj.imageUrl
+										? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+										: NoImage
+								}
+								user={obj.user}
+								createdAt={obj.createdAt}
+								viewsCount={obj.viewsCount}
+								commentsCount={3}
+								tags={obj.tags}
+								isEditable={userData?._id === obj.user._id}
+							/>
+						)
 					)}
 				</Grid>
 				<Grid xs={4} item>
@@ -57,20 +55,18 @@ export const Home = () => {
 						items={[
 							{
 								user: {
-									fullName: "James Rodrigues",
-									avatarUrl:
-										"https://mui.com/static/images/avatar/1.jpg",
+									fullName: "Вася Пупкин",
+									avatarUrl: "https://mui.com/static/images/avatar/1.jpg"
 								},
-								text: "This is the text comment",
+								text: "Это тестовый комментарий"
 							},
 							{
 								user: {
-									fullName: "Michel Kean",
-									avatarUrl:
-										"https://mui.com/static/images/avatar/2.jpg",
+									fullName: "Иван Иванов",
+									avatarUrl: "https://mui.com/static/images/avatar/2.jpg"
 								},
-								text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
-							},
+								text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top"
+							}
 						]}
 						isLoading={false}
 					/>
