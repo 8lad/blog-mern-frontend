af434128164c4f76abee258710d30446e32628ca
Vladyslav Troian <vladyslav.troian@cyper.ca>
2023-01-31 11:27:36 +0200

    fix: added styles for the comments block

--- src/components/CommentsBlock.jsx
+++ src/components/CommentsBlock.jsx
@@ -16,69 +16,83 @@ import { fetchComments } from "../redux/slices/comments";
 import { SideBlock } from "./SideBlock";
 
 export const CommentsBlock = ({ postId }) => {
-
 	const dispatch = useDispatch();
-	const { status, comments } = useSelector(state => state.comments);
+	const { status, comments } = useSelector((state) => state.comments);
 	const isLoading = status === "loading";
-	const currentComments = !postId ? comments : comments.filter(comment => comment.postId === postId);
+	const currentComments = !postId
+		? comments
+		: comments.filter((comment) => comment.postId === postId);
 
 	useEffect(() => {
 		dispatch(fetchComments());
 	}, []);
 
 	return (
-		<SideBlock title="Comments" >
-			{currentComments.length ? (<List sx={{maxHeigth: "500px", overflowY: "auto"}}>
-				{(isLoading ? [...Array(5)] : currentComments).map((obj, index) => (
-					<React.Fragment key={index}>
-						<ListItem alignItems="flex-start">
-							<ListItemAvatar>
-								{isLoading ? (
-									<Skeleton
-										variant="circular"
-										width={40}
-										height={40}
-									/>
-								) : (
-									<Avatar
-										alt={obj.user.fullName}
-										src={obj.user.avatarUrl ?? NoAvatar}
-									/>
-								)}
-							</ListItemAvatar>
-							{isLoading ? (
-								<div
-									style={{
-										display: "flex",
-										flexDirection: "column",
-									}}
-								>
-									<Skeleton
-										variant="text"
-										height={25}
-										width={120}
-									/>
-									<Skeleton
-										variant="text"
-										height={18}
-										width={230}
-									/>
-								</div>
-							) : (
-								<ListItemText
-									primary={obj.user.fullName}
-									secondary={obj.text}
-								/>
-							)}
-						</ListItem>
-						<Divider variant="inset" component="li" />
-					</React.Fragment>
-				))}
-			</List>) : <Typography variant="h5" component="h5" sx={{
-				padding: "10px 20px 30px 15px"
-			}}>
-				You don`t have any comments yet
-			</Typography>}
+		<SideBlock title="Comments">
+			{currentComments.length ? (
+				<List sx={{ maxHeight: "500px", overflowY: "auto" }}>
+					{(isLoading ? [...Array(5)] : currentComments).map(
+						(obj, index) => (
+							<React.Fragment key={index}>
+								<ListItem alignItems="flex-start">
+									<ListItemAvatar>
+										{isLoading ? (
+											<Skeleton
+												variant="circular"
+												width={40}
+												height={40}
+											/>
+										) : (
+											<Avatar
+												alt={obj.user.fullName}
+												src={
+													obj.user.avatarUrl ??
+													NoAvatar
+												}
+											/>
+										)}
+									</ListItemAvatar>
+									{isLoading ? (
+										<div
+											style={{
+												display: "flex",
+												flexDirection: "column",
+											}}
+										>
+											<Skeleton
+												variant="text"
+												height={25}
+												width={120}
+											/>
+											<Skeleton
+												variant="text"
+												height={18}
+												width={230}
+											/>
+										</div>
+									) : (
+										<ListItemText
+											primary={obj.user.fullName}
+											secondary={obj.text}
+										/>
+									)}
+								</ListItem>
+								<Divider variant="inset" component="li" />
+							</React.Fragment>
+						)
+					)}
+				</List>
+			) : (
+				<Typography
+					variant="h5"
+					component="h5"
+					sx={{
+						padding: "10px 20px 30px 15px",
+					}}
+				>
+					You don`t have any comments yet
+				</Typography>
+			)}
 			{postId && <Index postId={postId} />}
 		</SideBlock>
 	);
